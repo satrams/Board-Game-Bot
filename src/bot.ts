5ad3948f2152ab2c3e091cbc33770d8f4a71f5b7
satrams <mememaster420noscope360@gmail.com>
2022-08-14 19:37:08 -0400

    Revert "Catch-all error implementation for hooks"
    This reverts commit c14c39e8561662768ae4ebdcd38a97c9b9231910.

--- src/bot.ts
+++ src/bot.ts
@@ -19,15 +19,7 @@ const voter = new Voter();
 const scheduler = new Scheduler();
 const showcaser = new Showcaser();
 
-const catchErrors = <T extends any[]>(handler: (...args: T) => Promise<any>) => async (...args: T) => {
-	try {
-		return handler(...args);
-	} catch (e) {
-		console.error(e);
-	}
-};
-
-client.on('messageCreate', catchErrors(async (message) => {
+client.on('messageCreate', async (message) => {
 	// Specific behaviour for if the message occured in a specific channel
 	const channelIntercepts: Map<String, Interceptor> = new Map<String, Interceptor>([
 		[CHANNELLIST.voteChannel, voter],
@@ -38,9 +30,9 @@ client.on('messageCreate', catchErrors(async (message) => {
 	let interceptor = channelIntercepts.get(message.channelId);
 	console.log(interceptor);
 	interceptor?.interceptMessage(message);
-}));
+});
 
-client.on('interactionCreate', catchErrors(async (interaction) => {
+client.on('interactionCreate', interaction => {
 	if (interaction.type == InteractionType.ModalSubmit) {
 		const modalIntercepts: Map<String, Interceptor> = new Map<String, Interceptor>([
 			['Scheduler Set Amount Modal', scheduler]
@@ -61,21 +53,21 @@ client.on('interactionCreate', catchErrors(async (interaction) => {
 	}
 
 
-}));
+});
 
-client.on('messageReactionAdd', catchErrors(async (reaction) => {
+client.on('messageReactionAdd', async reaction => {
 
 	reaction = reaction.partial ? await reaction.fetch() : reaction; //If the reaction is a partial, reconstruct it first, and then continue with the rest of the function.
 
 	const reactionChannelIntercepts: Map<String, Interceptor> = new Map<String, Interceptor>([
 		[CHANNELLIST.voteChannel, voter],
-	]);
+	])
 
 	let interceptor = reactionChannelIntercepts.get(reaction.message.channelId);
 	console.log(interceptor);
 	interceptor?.interceptReaction(reaction as MessageReaction);
 
-}));
+});
 
 // Always keep this at the end of this file for simplicity
 client.login(token);
